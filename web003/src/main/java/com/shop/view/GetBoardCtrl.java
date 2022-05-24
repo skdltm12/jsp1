7429f918a3c4b0254fc8177eb2113bbec91d935d
skdltm12 <slsds@naver.com>
2022-05-24 11:23:05 +0900

    complete0524

--- web003/src/main/java/com/shop/view/GetBoardCtrl.java
+++ web003/src/main/java/com/shop/view/GetBoardCtrl.java
@@ -12,31 +12,26 @@ import javax.servlet.http.HttpServletResponse;
 import com.shop.common.BoardVO;
 import com.shop.model.BoardDAO;
 
-
 @WebServlet("/GetBoardCtrl")
 public class GetBoardCtrl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-
     public GetBoardCtrl() {
         super();
     }
-
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		int num = Integer.parseInt(request.getParameter("num"));
 		BoardDAO dao = new BoardDAO();
 		BoardVO board = new BoardVO();
-		System.out.println(num);
 		board = dao.getBoard(num);
 		if(board != null) {
-			request.setAttribute("vo", board);
-			RequestDispatcher view = request.getRequestDispatcher("getBoard.jsp");
-		}else {
+			request.setAttribute("board", board);
+			RequestDispatcher view = request.getRequestDispatcher("./board/getBoard.jsp");
+			view.forward(request, response);
+		} else {
 			response.sendRedirect("GetBoardListCtrl");
 		}
 	}
-
-
 }
